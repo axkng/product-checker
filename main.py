7c79df137ed00b41c8ec680de734fb79169e5036
Furragen <git@axk.io>
2025-07-29 15:52:51 +0200

    prevent caching

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class ProductChecker:
         self.session = requests.Session()
         self.session.headers.update(
             {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0",
+                "Cache-Control": "no-cache, no-store, must-revalidate",
+                "Pragma": "no-cache",
+                "Expires": "0",
             }
         )
 
